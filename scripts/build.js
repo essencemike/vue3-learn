b7b4bf32431b54bd5fd7351035a1c42489c58546
IMike <gzxessence@163.com>
2020-06-01 19:12:35 +0800

    feat: 初始化项目以及构建脚本

---
+++ scripts/build.js
@@ -0,0 +1,74 @@
+/*
+Produces production builds and stitches together d.ts files
+
+To specify the package to build, simply pass its name and the desired build
+formats to output (defaults to 'buildOptions.formats' specified in that package,
+or "esm,cjs"):
+
+```
+# name supports fuzzy match. will build all packages with name containing "dom":
+yarn build dom
+
+
+# specify the format to output
+yarn build core --formats cjs
+```
+*/
+
+const fs = require('fs-extra')
+const path = require('path')
+const chalk = require('chalk')
+const execa = require('execa')
+const { gzipSync } = require('zlib')
+const { compress } = require('brotli')
+const { targets: allTargets, fuzzyMatchTarget } = require('./utils')
+
+const args = require('minimist')(process.argv.slice(2))
+const targets = args._
+const formats = args.formats || args.f
+const devOnly = args.devOnly || args.d
+const prodOnly = !devOnly && (args.prodOnly || args.p)
+const sourceMap = args.sourcemap || args.s
+const isRelease = args.release
+const buildTypes = args.t || args.types || isRelease
+const buildAllMatching = args.all || args.a
+const commit = execa.sync('git', ['rev-parse', 'HEAD']).stdout.slice(0, 7)
+
+run()
+
+async function run() {
+  if (!targets.length) {
+    await buildAll(allTargets)
+    checkAllSizes(allTargets)
+  } else {
+    await buildAll(fuzzyMatchTarget(targets, buildAllMatching))
+    checkAllSizes(fuzzyMatchTarget(targets, buildAllMatching))
+  }
+}
+
+async function buildAll(targets) {
+  for (const target of targets) {
+    await build(target)
+  }
+}
+
+async function build(target) {
+  const pkgDir = path.resolve(`packages/${target}`)
+  const pkg = require(`${pkgDir}/package.json`)
+
+  // only build published packages for release
+  if (isRelease && pkg.private) {
+    return
+  }
+
+  // if building a specific format, do not remove dist.
+  if (!formats) {
+    await fs.remove(`${pkgDir}/dist`)
+  }
+
+  const env =
+    (pkg.buildOptions && pkg.buildOptions.env) ||
+    (devOnly ? 'development' : 'production')
+
+  await execa('rollup', ['-c', '--environment', []])
+}
