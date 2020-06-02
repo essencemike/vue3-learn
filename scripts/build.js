e74b1a62686bf21a1db15c063ec1620e2ba303f2
IMike <gzxessence@163.com>
2020-06-02 11:43:43 +0800

    ci: 添加 build 以及 release 的构建脚本

--- scripts/build.js
+++ scripts/build.js
@@ -70,5 +70,102 @@ async function build(target) {
     (pkg.buildOptions && pkg.buildOptions.env) ||
     (devOnly ? 'development' : 'production')
 
-  await execa('rollup', ['-c', '--environment', []])
+  await execa(
+    'rollup',
+    [
+      '-c',
+      '--environment',
+      [
+        `COMMIT:${commit}`,
+        `NODE_ENV:${env}`,
+        `TARGET:${target}`,
+        formats ? `FORMATS:${formats}` : '',
+        buildTypes ? `TYPES:true` : '',
+        prodOnly ? `PROD_ONLY:true` : '',
+        sourceMap ? `SOURCE_MAP:true` : ''
+      ]
+        .filter(Boolean)
+        .join(',')
+    ],
+    { stdio: 'inherit' }
+  )
+
+  if (buildTypes && pkg.types) {
+    console.log()
+    console.log(
+      chalk.bold(chalk.yellow(`Rolling up type definitions for ${target}...`))
+    )
+
+    // build types
+    const { Extractor, ExtractorConfig } = require('@microsoft/api-extractor')
+
+    const extractorConfigPath = path.resolve(pkgDir, 'api-extractor.json')
+    const extractorConfig = ExtractorConfig.loadFileAndPrepare(
+      extractorConfigPath
+    )
+    const extractorResult = Extractor.invoke(extractorConfig, {
+      localBuild: true,
+      showVerboseMessages: true
+    })
+
+    if (extractorResult.succeeded) {
+      // concat additional d.ts to rolled-up dts
+      const typesDir = path.resolve(pkgDir, 'types')
+      if (await fs.exists(typesDir)) {
+        const dtsPath = path.resolve(pkgDir, pkg.types)
+        const existing = await fs.readFile(dtsPath, 'utf-8')
+        const typeFiles = await fs.readdir(typesDir)
+        const toAdd = await Promise.all(
+          typeFiles.map(file => {
+            return fs.readFile(path.resolve(typesDir, file), 'utf-8')
+          })
+        )
+        await fs.writeFile(dtsPath, existing + '\n' + toAdd.join('\n'))
+      }
+      console.log(
+        chalk.bold(chalk.green('API Extractor completed successfully.'))
+      )
+    } else {
+      console.error(
+        `API Extractor completed with ${extractorResult.errorCount} errors` +
+          ` and ${extractorResult.warningCount} warnings`
+      )
+      process.exitCode = 1
+    }
+
+    await fs.remove(`${pkgDir}/dist/packages`)
+  }
+}
+
+function checkAllSizes(targets) {
+  if (devOnly) {
+    return
+  }
+  console.log()
+  for (const target of targets) {
+    checkSize(target)
+  }
+  console.log()
+}
+
+function checkSize(target) {
+  const pkgDir = path.resolve(`packages/${target}`)
+  checkFileSize(`${pkgDir}/dist/${target}.global.prod.js`)
+}
+
+function checkFileSize(filePath) {
+  if (!fs.existsSync(filePath)) {
+    return
+  }
+  const file = fs.readFileSync(filePath)
+  const minSize = (file.length / 1024).toFixed(2) + 'kb'
+  const gzipped = gzipSync(file)
+  const gzippedSize = (gzipped.length / 1024).toFixed(2) + 'kb'
+  const compressed = compress(file)
+  const compressedSize = (compressed.length / 1024).toFixed(2) + 'kb'
+  console.log(
+    `${chalk.gray(
+      chalk.bold(path.basename(filePath))
+    )} min:${minSize} / gzip:${gzippedSize} / brotli:${compressedSize}`
+  )
 }
